5ae987eb954c28f026e08048642d803cb03ce365
sssaiya <sssaiya@ucsd.edu>
2020-10-16 21:55:07 +0530

    Add log in

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
 const puppeteer = require("puppeteer");
 var creds = require("./constants");
 const profileUrlToScrape = "https://www.instagram.com/fuuuckingyoung/?hl=en";
+const loginURL = "https://www.instagram.com/accounts/login/";
 
 (async () => {
   console.log("Launching pupeteer");
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await logIn(page);
-  await page.goto(profileUrlToScrape);
-  await page.screenshot({ path: "img/example.png" });
+  // await page
+  //   .waitForFunction(
+  //     'document.querySelectorAll("button")[1].inner‌​Text.includes("Not Now")'
+  //   )
+  //   .catch((err) => console.log(err));
+  console.log("Logged in");
+  // page.waitForNavigation({ waitUntil: 'networkidle0' }),
+  await page.screenshot({ path: "img/afterLOGIN.png" });
+  // await page.goto(profileUrlToScrape);
+  // await page.screenshot({ path: "img/example.png" });
   await browser.close();
 })();
 
-
-
 async function logIn(page) {
-    console.log(`Logging in as ${creds.USERNAME}`);
-    await page.goto("https://www.instagram.com/accounts/login/");
-    await page.waitForSelector('input[name="username"]');
-    await page.type('input[name="username"]', creds.USERNAME);
-    await page.type('input[name="password"]', creds.PASSWORD);
-    await page.screenshot({ path: "img/login.png" });
-    await page.click('button[type="submit"]');
-    await page.screenshot({ path: "img/submitted.png" });
+  console.log(`Logging in as ${creds.USERNAME}`);
+  await page.goto(loginURL);
+  await page.waitForSelector('input[name="username"]');
+  await page.type('input[name="username"]', creds.USERNAME);
+  await page.type('input[name="password"]', creds.PASSWORD);
+  // Submit log in credentials and wait for navigation
+  await Promise.all([
+    page.click('[type="submit"]'),
+    page.waitForNavigation({
+      waitUntil: "networkidle0",
+    }),
+  ]);
 }
